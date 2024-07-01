fa694384d9656dd4bf740573456a0306cfa4b22b
matiasdicarlo <107714048+matiasdicarlo@users.noreply.github.com>
2024-07-01 16:34:27 -0300

    Add files via upload

---
+++ app/autentificacion.php
@@ -0,0 +1,17 @@
+<?php session_start();
+require_once 'configuracion.php';
+
+// Autentificación de codigo Google OAuth 
+if (isset($_GET['code'])) {
+  $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+  $client->setAccessToken($token['access_token']);
+  
+  // obtener informacion de perfil
+  $google_oauth = new Google_Service_Oauth2($client);
+  $google_account_info = $google_oauth->userinfo->get();
+  $email =  $google_account_info->email;
+  $name =  $google_account_info->name;
+ 
+ 
+}
+?>
